e78bdb82e3c2bc7b713f9bfd8c38990590af9180
Scott Warner <Tortel1210@gmail.com>
2023-11-02 21:56:13 -0400

    shusky: overlay: Configure aux cameras for Aperture
    Change-Id: I3d1c93057b5c2a1ba09c06a945d538d2043f30fd

--- husky/device-lineage.mk
+++ husky/device-lineage.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/husky/overlay-lineage
+
 TARGET_VENDOR_PROP += device/google/shusky/husky/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-lineage.mk)
--- shiba/device-lineage.mk
+++ shiba/device-lineage.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay-lineage
+
 TARGET_VENDOR_PROP += device/google/shusky/shiba/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-lineage.mk)
